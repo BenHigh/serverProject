45e1afffffe75e76cdb80b18ecb5efd025416f13
BenHigh <ben.high@austin.rr.com>
2017-11-09 13:19:09 -0600

    added port listener for customer app

--- server.js
+++ server.js
@@ -10,11 +10,18 @@ var sid = require("shortid");
 // Sets up the Express App
 // =============================================================
 var app = express();
+
+var cApp = express();
+var CPORT = process.env.PORT || 3001;
+
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+cApp.use(bodyParser.urlencoded({ extended: false }));
+cApp.use(bodyParser.json());
 
 // Star Wars Characters (DATA)
 // =============================================================
@@ -90,8 +97,32 @@ app.post("/api/newReservation", function(req, res) {
   res.json(newReservation);
 });
 
+cApp.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "cAdd.html"));
+});
+
+cApp.post("/api/newReservation", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newReservation = req.body;
+  newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+
+  newReservation.uID = sid.generate();
+ 
+  console.log(newReservation);
+
+  reservation.push(newReservation);
+
+  res.json(newReservation);
+});
+
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
+});
+
+
+cApp.listen(CPORT, function() {
+  console.log("App listening on PORT " + CPORT);
 });
